94562b3e6ee6a81581ab77a3a2ed23e3b0f74cde
João Claudiano <46274604+JoaoClaudiano@users.noreply.github.com>
2026-02-02 15:11:24 -0300

    Add Home component with MapView integration

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MapView from "../components/MapView";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Mapa de espaços de prática ao ar livre</h1>
+      <MapView />
+    </div>
+  );
+};
+
+export default Home;
